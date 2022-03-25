0eb5086e7e7245d5e558262ba64e7b054522e956
Marta M Aguilera <73697174+martamagui@users.noreply.github.com>
2022-03-25 11:15:09 +0100

    edit-delete task dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,15 +19,35 @@ export class AppComponent {
       description: 'Go to the store and buy milk',
     },
     {
-      title: 'Create a Kanban app',
-      description: 'Using Firebase and Angular create a Kanban app!',
+      title: 'Create a TodoApp app',
+      description: 'Using Firebase and Angular create a TodoApp app!',
     },
   ];
   inProgress: Task[] = [];
   done: Task[] = [];
   constructor(private dialog: MatDialog) {}
 
-  editTask(list: string, task: Task): void {}
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '30vw',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult|undefined) => {
+      if (!result) {
+        return;
+      }
+      const dataList = this[list];
+      const taskIndex = dataList.indexOf(task);
+      if (result.delete) {
+        dataList.splice(taskIndex, 1);
+      } else {
+        dataList[taskIndex] = task;
+      }
+    });
+  }
 
   drop(event: CdkDragDrop<Task[]>): void {
     if (event.previousContainer === event.container) {
